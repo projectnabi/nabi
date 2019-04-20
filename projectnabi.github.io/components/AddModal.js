fa7bc086d30f686aa84e599d37522aa13e334656
Minsu Gim <minsugim@uw.edu>
2019-04-20 01:07:30 -0700

    Added project card

--- projectnabi.github.io/components/AddModal.js
+++ projectnabi.github.io/components/AddModal.js
@@ -1,71 +1,55 @@
 import React, { Component, } from 'react';
-import { Alert, AppRegistry, Platform, StyleSheet, Text, TextInput, Image, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, Dimensions } from 'react-native';
+import { Alert, AppRegistry, Button, Platform, StyleSheet, Text, TextInput, Image, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View, Dimensions } from 'react-native';
 import Modal from 'react-native-modalbox'
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 export default class AddModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          title : null,
+        };
+      }
     componentWillMount() {
         this.setState({ color: this.props.color })
     }
     showAddModal = () => {
         this.refs.addModal.open()
     }
-
     render() {
         return (
             <Modal backdropPressToClose={false} backButtonClose={true}
                 ref={"addModal"}
                 style={{
-                    justifyContent: 'space-evenly',
-                    alignItems: 'center',
-                    borderRadius: 5,
-                    shadowRadius: 10,
-                    width: Dimensions.get('window').width - 20,
-                    //  height: 280,
-                    flex: .9
+                    justifyContent: 'flex-start',
+                    alignItems: 'center'
                 }}
                 position='center'
                 backdrop={true}
-                onClosed={() => {
-                    // alert("MODAL CLOSED")
-                }}>
-                <TouchableOpacity
-                    style={styles.close}>
-                    <Ionicons name="ios-close" size={40} color="black" />
+                visible={this.state.visibleModal}
+                >
+                <TouchableOpacity style={styles.close}>
+                    <Ionicons name="ios-arrow-down" size={40} color="black" />
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.more}>
                     <Ionicons name="md-more" size={24} color="black" />
                 </TouchableOpacity>
-                <Text style={{ fontSize: 25 }}>Add Project!</Text>
-                <Image style = {{width: 120,height: 120, resizeMode: 'contain'}} source = {require("../assets/EGG.png")}/>
+                <Image style = {{width: 120,height: 120, resizeMode: 'contain', alignSelf: 'center'}} source = {require("../assets/EGG.png")}/>
+                <View style={{alignSelf: 'stretch', paddingLeft: 37, paddingTop:20, paddingBottom: 10}}>
+                    <Text style={styles.projectTitle}>PROJECT NAME</Text>
+                </View>
                 <TextInput style={styles.input}
                     underlineColorAndroid="transparent"
                     placeholder="Title"
                     placeholderTextColor="gray"
                     autoCapitalize="none"
-                    onChangeText={this.handleEmail} />
-
-                <TextInput style={styles.input}
-                    underlineColorAndroid="transparent"
-                    placeholder="Bird Name"
-                    placeholderTextColor="gray"
-                    autoCapitalize="none"
-                    onChangeText={this.handlePassword} />
-
-                <TextInput style={styles.input}
-                    underlineColorAndroid="transparent"
-                    placeholder="Frequency"
-                    placeholderTextColor="gray"
-                    autoCapitalize="none"
-                    onChangeText={this.handlePassword} />
-
+                    onChangeText={(text) => this.setState({text})} />
                 <TouchableOpacity
-                    style={styles.submitButton}
-                >
+                    style={styles.submitButton}>
+                    
                     <Text style={styles.submitButtonText}> Done </Text>
                 </TouchableOpacity>
-
             </Modal>
         );
     }
@@ -76,12 +60,13 @@ const styles = StyleSheet.create({
         paddingTop: 23
     },
     input: {
-        margin: 10,
+        fontSize: 40,
         width: 300,
-        borderRadius: 5,
         height: 40,
-        borderColor: 'gray',
-        borderWidth: 1
+        padding: 0,
+    },
+    projectTitle: {
+        fontSize: 12,
     },
     submitButton: {
         backgroundColor: '#ceeeb0',
@@ -90,8 +75,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         margin: 15,
         height: 40,
-        width: 300,
-        borderRadius: 5
+        width: 150,
+        borderRadius: 5,
+        borderBottomColor: 'green'
     },
     submitButtonText: {
         color: 'white',
@@ -102,13 +88,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: 70,
-        position: 'absolute',
         // shadowColor: '#000',
         // shadowOffset: { width: 0, height: 2 },
         // shadowOpacity: 0.8,
         // shadowRadius: 2,
-        top: 10,
-        left: 10,
+        paddingTop: 20,
         height: 70,
         backgroundColor: '#fff',
         borderRadius: 100,
@@ -129,5 +113,6 @@ const styles = StyleSheet.create({
          height: 70,
          backgroundColor: '#fff',
          borderRadius: 100,
+         paddingBottom: 40,
        }
 })

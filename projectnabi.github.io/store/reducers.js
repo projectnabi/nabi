ea173f316ee77dfc3dc32f9b3d058a5162298a36
jchou8 <joseph98c@gmail.com>
2019-05-02 02:19:28 -0700

    handling day changes, tracking completions/streaks

--- projectnabi.github.io/store/reducers.js
+++ projectnabi.github.io/store/reducers.js
@@ -9,11 +9,27 @@ const defaultSettings = {
 function projectList(state = {}, action) {
     switch (action.type) {
         case actions.ADD_PROJECT:
-            return update(state, {[action.id]: {$set: action.project}})
+            return update(state, { [action.id]: { $set: action.project } })
         case actions.SET_TIME:
-            return update(state, {[action.id]: {markedDates: {[action.date]: {$set: action.time}}}})
+            return update(state, { [action.id]: { markedDates: { [action.date]: { $set: action.time } } } })
         case actions.MARK_DONE:
-            return update(state, {[action.id]: {completions: {$set: !state.completions && 1 || (state.completedToday && state.completions || state.completions + 1)}, completedToday: {$set: true}}})
+            return update(state, { [action.id]: { completedToday: { $set: true } } })
+        case actions.INCR_STREAK:
+            let curState = state[action.id]
+            let nextCompletions = !curState.completions && 1 || curState.completions + 1
+            let nextStreak = !curState.currentStreak && 1 || curState.currentStreak + 1
+            let nextBest = !curState.bestStreak && nextStreak || Math.max(nextStreak, curState.bestStreak)
+            return update(state, {
+                [action.id]: {
+                    completions: { $set: nextCompletions },
+                    currentStreak: { $set: nextStreak },
+                    bestStreak: { $set: nextBest }
+                }
+            })
+        case actions.RESET_STREAK:
+            return update(state, { [action.id]: { currentStreak: { $set: 0 } } })
+        case actions.MARK_INCOMPLETE:
+            return update(state, { [action.id]: { completedToday: { $set: false } } })
         default:
             return state
     }
@@ -22,7 +38,17 @@ function projectList(state = {}, action) {
 function settings(state = defaultSettings, action) {
     switch (action.type) {
         case actions.UPDATE_SETTING:
-            return update(state, {[action.setting]: {$set: action.value}})
+            return update(state, { [action.setting]: { $set: action.value } })
+        default:
+            return state
+    }
+}
+
+
+function user(state = {}, action) {
+    switch (action.type) {
+        case actions.UPDATE_LAST_SEEN:
+            return update(state, { lastSeen: { $set: action.time } })
         default:
             return state
     }
@@ -30,7 +56,8 @@ function settings(state = defaultSettings, action) {
 
 const baseReducer = combineReducers({
     projectList,
-    settings
+    settings,
+    user
 })
 
 export default baseReducer

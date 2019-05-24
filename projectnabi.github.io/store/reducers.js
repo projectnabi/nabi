b44873e2e70af1ff160cd9ace8658ed89c1bb4be
jchou8 <joseph98c@gmail.com>
2019-05-24 12:46:12 -0700

    handle health

--- projectnabi.github.io/store/reducers.js
+++ projectnabi.github.io/store/reducers.js
@@ -11,7 +11,7 @@ function projectList(state = {}, action) {
         case actions.ADD_PROJECT:
             return update(state, { [action.id]: { $set: action.project } })
         case actions.DELETE_PROJECT:
-            return update(state, { [action.id]: { $set: null } })
+            return update(state, { $unset: [action.id] })
         case actions.SET_TIME:
             return update(state, { [action.id]: { markedDates: { [action.date]: { $set: action.time } } } })
         case actions.MARK_DONE:
@@ -25,7 +25,8 @@ function projectList(state = {}, action) {
                 [action.id]: {
                     completions: { $set: nextCompletions },
                     currentStreak: { $set: nextStreak },
-                    bestStreak: { $set: nextBest }
+                    bestStreak: { $set: nextBest },
+                    health: { $set: Math.min(curState.health + 10, 100) }
                 }
             })
         case actions.RESET_STREAK:
@@ -36,6 +37,9 @@ function projectList(state = {}, action) {
             return update(state, { [action.id]: { schedule: { $set: action.schedule } } })
         case actions.HATCH:
             return update(state, { [action.id]: { img: { $set: 'bird' + (Math.floor(Math.random() * 2) + 1) } } })
+        case actions.DAMAGE_BIRD:
+            let newHealth = Math.max(state[action.id].health - action.amount, 0)
+            return update(state, { [action.id]: { health: { $set: newHealth } } })
         default:
             return state
     }

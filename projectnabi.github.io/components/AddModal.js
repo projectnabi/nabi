22fe07b9673ef919003861efce37a9707f157fec
jchou8 <joseph98c@gmail.com>
2019-06-01 16:07:29 -0700

    fix markeddates not initializing

--- projectnabi.github.io/components/AddModal.js
+++ projectnabi.github.io/components/AddModal.js
@@ -59,7 +59,7 @@ class AddModal extends Component {
             if (this.state.edit) {
                 this.props.dispatch(updateProject(Date.now(), { title: this.state.title, name: this.state.name, days: this.getDays()}))
             } else {
-                this.props.dispatch(addProject(Date.now(), { title: this.state.title, name: this.state.name, img: 'egg', health: 100, days: this.getDays()}))
+                this.props.dispatch(addProject(Date.now(), { title: this.state.title, name: this.state.name, img: 'egg', health: 100, days: this.getDays(), markedDates: []}))
             }
             this.setState({name:"", title:"", days: []})
             this.closeModal()
